e15f79c46b10f5ac57bea4fb326d6d099221242e
Sam Shankland <sjs227@uclive.ac.nz>
2021-10-19 09:28:23 +1300

    Boilerplate code

---
+++ src/main/java/nz/ac/canterbury/dataprovenancedemo/DataProvenanceDemoApplication.java
@@ -0,0 +1,13 @@
+package nz.ac.canterbury.dataprovenancedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProvenanceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProvenanceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nz/ac/canterbury/dataprovenancedemo/DataProvenanceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.ac.canterbury.dataprovenancedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProvenanceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
