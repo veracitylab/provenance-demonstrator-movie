6eb244195b890cd785af3716dadc28b3429fa567
Sam Shankland <sjs227@uclive.ac.nz>
2021-11-22 13:33:12 +1300

    Converted to multiproject

--- web/src/main/java/nz/ac/canterbury/dataprovenancedemo/DataProvenanceDemoApplication.java
+++ web/src/main/java/nz/ac/canterbury/dataprovenancedemo/DataProvenanceDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
 import java.sql.SQLException;
 
 @SpringBootApplication
