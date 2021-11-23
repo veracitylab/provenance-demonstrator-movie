87f0d370bc8bbc22c0ac9e06151013e3baf377c5
Sam Shankland <sjs227@uclive.ac.nz>
2021-11-23 13:34:36 +1300

    Annotations

--- recommender/src/main/java/nz/ac/canterbury/dataprovenancedemo/annotations/AlgorithmProperty.java
+++ recommender/src/main/java/nz/ac/canterbury/dataprovenancedemo/annotations/AlgorithmProperty.java
@@ -4,13 +4,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-/**
- * This annotation set will be used to trace how an algorithm performs some action
- */
-@Target({METHOD, FIELD})
+@Target(FIELD)
 @Retention(RUNTIME)
-public @interface Trace {
+public @interface AlgorithmProperty {
 }
---
+++ recommender/src/main/java/nz/ac/canterbury/dataprovenancedemo/utils/AlgorithmAnnotationProcessor.java
@@ -0,0 +1,4 @@
+package nz.ac.canterbury.dataprovenancedemo.utils;
+
+public class AlgorithmAnnotationProcessor {
+}
