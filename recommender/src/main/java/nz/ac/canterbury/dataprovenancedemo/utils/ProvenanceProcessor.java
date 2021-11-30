945475e95c885aaa8f1da65cd743a89fb136978d
Sam Shankland <sjs227@uclive.ac.nz>
2021-11-30 13:47:32 +1300

    indent

--- recommender/src/main/java/nz/ac/canterbury/dataprovenancedemo/utils/ProvenanceProcessor.java
+++ recommender/src/main/java/nz/ac/canterbury/dataprovenancedemo/utils/ProvenanceProcessor.java
@@ -9,18 +9,18 @@ public class ProvenanceProcessor {
 
     protected ProvenanceProcessor() {}
 
-        public static void process(MovieRecommender target) {
-            Algorithm[] algoNames = target.getClass().getAnnotationsByType(Algorithm.class);
-            AlgorithmProperty[] algoProps = target.getClass().getAnnotationsByType(AlgorithmProperty.class);
-            AlgorithmStep[] algoSteps = target.getClass().getAnnotationsByType(AlgorithmStep.class);
+    public static void process(MovieRecommender target) {
+        Algorithm[] algoNames = target.getClass().getAnnotationsByType(Algorithm.class);
+        AlgorithmProperty[] algoProps = target.getClass().getAnnotationsByType(AlgorithmProperty.class);
+        AlgorithmStep[] algoSteps = target.getClass().getAnnotationsByType(AlgorithmStep.class);
 
-            assert algoNames.length == 1;
+        assert algoNames.length == 1;
 
-            Class<?> clazz = target.getClass();
+        Class<?> clazz = target.getClass();
 
-            System.out.printf("Algorithm information for class: %s%n", clazz.getSimpleName());
-            System.out.printf("\tName: %s%n", algoNames[0].name());
-            System.out.printf("\tDescription: %s%n", algoNames[0].description());
+        System.out.printf("Algorithm information for class: %s%n", clazz.getSimpleName());
+        System.out.printf("\tName: %s%n", algoNames[0].name());
+        System.out.printf("\tDescription: %s%n", algoNames[0].description());
     }
 
 }
