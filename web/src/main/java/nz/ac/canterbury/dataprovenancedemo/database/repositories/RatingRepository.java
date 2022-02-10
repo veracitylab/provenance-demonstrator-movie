a7a1a16a3f6dc50a043ef1e6c539214dade83251
Sam Shankland <sjs227@uclive.ac.nz>
2022-02-11 12:45:01 +1300

    Documentation

--- web/src/main/java/nz/ac/canterbury/dataprovenancedemo/database/repositories/RatingRepository.java
+++ web/src/main/java/nz/ac/canterbury/dataprovenancedemo/database/repositories/RatingRepository.java
@@ -3,6 +3,14 @@ package nz.ac.canterbury.dataprovenancedemo.database.repositories;
 import nz.ac.canterbury.dataprovenancedemo.database.model.Rating;
 import org.springframework.data.repository.Repository;
 
+/**
+ * Responsible for handling the insertion of ratings into the database.
+ */
 public interface RatingRepository extends Repository<Rating, Integer> {
+
+    /**
+     * Inserts a given rating into the database. Rating contains username, movieId and numerical rating.
+     * @param rating The rating object to save.
+     */
     void save(Rating rating);
 }
