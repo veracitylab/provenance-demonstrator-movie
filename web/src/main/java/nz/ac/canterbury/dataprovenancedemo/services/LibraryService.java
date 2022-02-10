7fde32f0690d48ed0fbaf244c96a21d2dc82941f
Sam Shankland <sjs227@uclive.ac.nz>
2022-02-11 12:36:21 +1300

    Documentation

--- web/src/main/java/nz/ac/canterbury/dataprovenancedemo/services/LibraryService.java
+++ web/src/main/java/nz/ac/canterbury/dataprovenancedemo/services/LibraryService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 /**
  * This class is responsible for handling actions relating to the retrieval of movies and
- * modification of movie ratings
+ * modification of movie ratings.
  */
 @Service
 public class LibraryService {
@@ -22,38 +22,50 @@ public class LibraryService {
     private final MovieRepository movieRepository;
     private final RatingRepository ratingRepository;
 
+
+    /**
+     * Constructs the service using DI for the movie repository and the rating repository.
+     * @param movieRepository Repository object used for retrieving movies.
+     * @param ratingRepository Repository object used for interacting with ratings.
+     */
     @Autowired
     public LibraryService(MovieRepository movieRepository, RatingRepository ratingRepository) {
         this.movieRepository = movieRepository;
         this.ratingRepository = ratingRepository;
     }
 
+
     /**
-     * Gets movies with optional pagination
-     * @param pageNum Page number to go to
-     * @return A page of unsorted movies
+     * Gets a page of movies at the specified page number.
+     * @param pageNum Page number to get movies from.
+     * @return A page of unsorted movies.
      */
     public Page<Movie> getMovies(int pageNum) {
         return movieRepository.findAll(PageRequest.of(pageNum, DEFAULT_PAGE_SIZE));
     }
 
+    /**
+     * Gets a movie with a specific ID.
+     * @param id ID of the movie to get
+     * @return The movie if the ID is present, empty optional if not.
+     */
     public Optional<Movie> getMovie(int id) {
         return movieRepository.findMovieById(id);
     }
 
     /**
-     * Searches the movie database, including ability to paginate
-     * @param pageNum Page number to query
-     * @param title Title to search by, the case is ignored
-     * @return A Page of movies filtered by their title
+     * Searches the movie database, including ability to paginate.
+     * @param pageNum Page number to query.
+     * @param title Title to search by, the case is ignored.
+     * @return A Page of movies filtered by their title.
      */
     public Page<Movie> getMoviesByTitle(int pageNum, String title) {
         return movieRepository.findMovieByTitleContainingIgnoreCase(title, PageRequest.of(pageNum, DEFAULT_PAGE_SIZE));
     }
 
     /**
-     * Inserts a given rating into the database
-     * @param rating The rating with relevant information to be inserted
+     * Inserts a given rating into the database.
+     * @param rating The rating with relevant information to be inserted.
      */
     public void rateMovie(Rating rating) {
         ratingRepository.save(rating);
